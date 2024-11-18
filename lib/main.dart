ed448c964df274b36c23a9cb19cdb035b5f20db9
André Scheiermann <scheiermann.andre@gmail.com>
2024-11-18 11:20:30 +0100

    remove todo text in the main file

--- lib/main.dart
+++ lib/main.dart
@@ -8,35 +8,6 @@ import 'package:riverpod_annotation/riverpod_annotation.dart';
 
 part 'main.g.dart';
 
-// DONE
-// Graph and Data Visualization
-// each with own Tab and user can switch between them
-// as line chart
-// include Data Preloading
-// date filtering
-// support Unit Switching
-// support Data Downsampling
-// support Data Caching
-// add option to clear cache
-// Pull-to-refresh functionality.
-// Implement user-friendly error messages for connectivity issues or request failures.
-// Dark mode support
-// Datapolling to automatically refresh data
-// Include unit tests for core business logic.
-// Include widget tests for UI components and interactions.
-// Instructions for running the application.
-
-// TODO:
-// TODO: Ensure to include detailed documentation for the solution.
-// TODO: A short description of trade-offs or design choices made due to time constraints.
-
-// Functional Requirements Compliance: Does the solution meet thefunctional requirements?
-// • CodeQuality & Structure: Is the code readable, and modular? Are architecture and statemanagemen twell-applied?
-// • Testing: Quality and coverage of unit and widget tests.
-// • ErrorHandling: Are error smanaged with user-friendly feedback?
-// • Performance: Effective caching and preloading of data.
-// • Bonus: Implementation of nice-to-have features like pull-to-refresh, dark mode, and data polling.
-
 void main() {
   runApp(
     const ProviderScope(child: App()),
